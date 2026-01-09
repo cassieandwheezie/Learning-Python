4cfe289f9b484298925e1b72e7743bc669960150
Manmeet Kaur <mkmanmeet6649@gmail.com>
2026-01-09 22:49:08 +0530

    Create Input.py
    Prompting to take input from user and displaying their residence

---
+++ Input.py
@@ -0,0 +1,24 @@
+'''
+
+Write a C++ program that prompts the user to enter their name and city. The
+program should then display the following formatted output:
+Name: [user's name]
+City: [user's city]
+Country: India
+Replace [user's name] and [user's city] with the actual inputs provided by the user.
+
+'''
+
+
+print("*** Entering the world of Python — where logic meets simplicity *** ")
+print("Enter your name: ")
+name = input()
+print("Enter your city: ")
+city = input()
+print()
+print("Thanks for providing your information.")
+print("Printing your details:")
+print()
+print(f'Name: {name}')
+print(f'City: {city}')
+print('Country: India')
